dc35ac83c4c61d86e3acf56e94d2b2d9d711a3ea
Felipe Alves <felipealves1550@gmail.com>
2024-08-15 21:14:48 -0300

    Corrigindo CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://felipe-3583:5174', 'http://notedash.tech'],
 
     'allowed_origins_patterns' => [],
 
